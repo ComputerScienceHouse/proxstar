c2c91b9f37f0f11c6b0634fd0cf3702b741a9890
Jordan Rodgers <com6056@gmail.com>
2018-02-11 22:02:06 -0500

    remove secrets module for generating passwords until this works with python 3.6

--- proxstar/util.py
+++ proxstar/util.py
@@ -1,8 +1,7 @@
-import secrets
+import string
+import random
 
 
-def gen_password(
-        length,
-        charset="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"
-):
-    return "".join([secrets.choice(charset) for _ in range(0, length)])
+def gen_password(length, charset="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"):
+    # use secrets module once this works in python 3.6
+    return ''.join(random.choice(charset) for x in range(length))
