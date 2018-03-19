f189b00932e67b6b01349b8cd7beac43e0ca1ea6
Jordan Rodgers <com6056@gmail.com>
2018-03-19 14:27:11 -0400

    add logging to debug boot order issue on openshift

--- proxstar/static/js/script.js
+++ proxstar/static/js/script.js
@@ -988,6 +988,8 @@ $("#edit-boot-order").click(function(){
             var data  = new FormData();
             for (k = 0; k < boot_order.length; k++) {
                 e = document.getElementById(`boot-order-${k + 1}`);
+                console.log(k + 1);
+                console.log(e.options[e.selectedIndex].value);
                 data.append(`${k + 1}`, e.options[e.selectedIndex].value);
             }
             fetch(`/vm/${vmid}/boot_order`, {
