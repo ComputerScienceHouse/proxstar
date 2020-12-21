d48a4cf90aded41b708e10252b71a10f247ed93a
Devin Matte <devinmatte@gmail.com>
2020-12-21 14:17:48 -0500

    Adding Redis Integration

--- rqsettings.py
+++ rqsettings.py
@@ -2,14 +2,15 @@ import os
 
 import sentry_sdk
 from sentry_sdk.integrations.rq import RqIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
 
-if os.path.exists('config_local.py'):
+if os.path.exists("config_local.py"):
     import config_local as config
 else:
     import config
 
 sentry_sdk.init(
     config.SENTRY_DSN,
-    integrations=[RqIntegration()],
-    environment=config.SENTRY_ENV
+    integrations=[RqIntegration(), RedisIntegration()],
+    environment=config.SENTRY_ENV,
 )
