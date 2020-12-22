ecd412797283c6ad1f1ab658b9488856ff9f75b3
Devin Matte <devinmatte@gmail.com>
2020-12-22 17:37:51 -0500

    Revert "Upgrading to pyoidc 3.7.0" (#78)

--- proxstar/auth.py
+++ proxstar/auth.py
@@ -1,17 +1,12 @@
 from flask_pyoidc.flask_pyoidc import OIDCAuthentication
-from flask_pyoidc.provider_configuration import ProviderConfiguration, ClientMetadata
 from tenacity import retry
 
 
 @retry
 def get_auth(app):
-    sso_config = ProviderConfiguration(
+    auth = OIDCAuthentication(
+        app,
         issuer=app.config['OIDC_ISSUER'],
-        client_metadata=ClientMetadata(
-            app.config['OIDC_CLIENT_CONFIG']['client_id'],
-            app.config['OIDC_CLIENT_CONFIG']['client_secret'],
-        ),
+        client_registration_info=app.config['OIDC_CLIENT_CONFIG'],
     )
-
-    auth = OIDCAuthentication({'sso': sso_config}, app)
     return auth
